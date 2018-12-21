40ae292ac91ed3eeff8aaef8430ec96ae93bd46e
lndubose <lndubose13@gmail.com>
2018-12-20 22:52:48 -0600

    Basic structure

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,10 +3,22 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
+import About from '../sections/about.js'
+import Jumbo from '../sections/jumbo.js'
+import Projects from '../sections/projects.js'
+import Experience from '../sections/experience.js'
+import Tech from '../sections/tech.js'
+
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi starter</h1>
-  </Layout>
+  <>
+    <Jumbo />
+    <Layout>
+      <About />
+      <Projects />
+      <Experience />
+      <Tech />
+    </Layout>
+  </>
 )
 
 export default IndexPage
---
+++ client/src/sections/jumbo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import JumboImage from '../images/jumbo1.jpg'
+
+export default () => {
+  return (
+    <div>
+      <img src={JumboImage} alt="" />
+      <h1>Lauren Dubose</h1>
+    </div>
+  )
+}
