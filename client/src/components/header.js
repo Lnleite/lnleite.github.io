9f5e39b3d9ae7542b13adb74ee06c9eac5ab5c70
lndubose <lndubose13@gmail.com>
2018-12-20 21:30:42 -0600

    Initialize project

---
+++ client/src/components/header.js
@@ -0,0 +1,17 @@
+// import { Link } from 'gatsby'
+// import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = ({ siteTitle }) => (
+  <div>
+    <h3>Home</h3>
+  </div>
+)
+
+Header.propTypes = {}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ client/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi starter</h1>
+  </Layout>
+)
+
+export default IndexPage
