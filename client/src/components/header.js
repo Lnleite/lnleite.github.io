a2020b91b35288e361523e46b78a46030dcd011b
lndubose <lndubose13@gmail.com>
2018-12-20 23:18:45 -0600

    Add Jumbo Picture

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,10 +2,19 @@
 // import PropTypes from 'prop-types'
 import React from 'react'
 
+import styled from 'styled-components'
+
+const Fix = styled.div`
+  position: fixed;
+  top: 0;
+  background: white;
+  width: 100%;
+`
+
 const Header = ({ siteTitle }) => (
-  <div>
+  <Fix>
     <h3>Home</h3>
-  </div>
+  </Fix>
 )
 
 Header.propTypes = {}
--- client/src/sections/jumbo.js
+++ client/src/sections/jumbo.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 import JumboImage from '../images/jumbo1.jpg'
+import styled from 'styled-components'
+
+const JumboContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${JumboImage});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+`
 
 export default () => {
   return (
-    <div>
-      <img src={JumboImage} alt="" />
+    <JumboContainer>
       <h1>Lauren Dubose</h1>
-    </div>
+    </JumboContainer>
   )
 }
