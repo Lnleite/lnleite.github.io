25b85c3248f1255990abf1d240a0579cf56d63df
Lauren D <lndubose13@gmail.com>
2018-12-30 19:30:48 -0600

    Add new Jumbo picture and style
    Changes the profile picture to be more plain and added the name and some descriptions to the banner, in the middle of the jumbo image.

--- client/src/sections/jumbo.js
+++ client/src/sections/jumbo.js
@@ -1,21 +1,37 @@
 import React from 'react'
 
-import JumboImage from '../images/jumbo1.jpg'
+import JumboImage from '../images/natureBackground.jpg'
 import styled from 'styled-components'
 
 const JumboContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 500px;
   background-image: url(${JumboImage});
   background-repeat: no-repeat;
-  background-size: 100%;
+  background-size: cover;
   background-position: center;
+  margin-top: 15px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: white;
+
+  h1 {
+    font-size: 2rem;
+  }
 `
 
 export default () => {
   return (
     <JumboContainer>
-      <h1>Lauren Dubose</h1>
+      <div>
+        <h1>Lauren Dubose</h1>
+        <p>Developer</p>
+        <p>Engineer</p>
+        <p>Coder</p>
+      </div>
     </JumboContainer>
   )
 }
