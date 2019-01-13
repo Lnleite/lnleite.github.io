4403d84bb7914dc46914b9252769a190f83ea7cd
Lauren D <lndubose13@gmail.com>
2019-01-13 15:19:34 -0600

    Remove gatsby
    Since I am doing the portfolio in vanilla js, I am removin gatsby.

--- client/src/sections/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  )
-}
--- client/src/sections/tech.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-  return (
-    <div>
-      <h2>Tech</h2>
-    </div>
-  )
-}
