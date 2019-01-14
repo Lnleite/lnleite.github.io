4041495762b100eb2ab664e586ac29ca5823d635
Lauren D <lndubose13@gmail.com>
2019-01-13 22:28:00 -0600

    Center text and update lazy load
    Trying to improve the blur to the high resolution picture effect. Still work in
    progress.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,11 @@
 let jumboImg = document.querySelector(".jumboImg");
 
-window.addEventListener("load", function() {
+const fakeImage = new Image();
+fakeImage.onload = function() {
+  jumboImg.styles.backgroundImage = `url(${fakeImage.src})`;
+};
+fakeImage.src = "images/jumboImg.jpg";
+
+fakeImage.addEventListener("load", function() {
   jumboImg.classList.remove("loading");
 });
