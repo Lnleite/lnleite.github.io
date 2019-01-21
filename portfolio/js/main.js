b6b85b426fe21f7af73019490973bdacee2b7f90
Lauren D <lndubose13@gmail.com>
2019-01-21 12:31:28 -0600

    Add scroll effect
    Added a scoll effect when scrolling down and up the page to give  a cool animation effect.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -9,3 +9,24 @@ fakeImage.src = "images/jumboImg.jpg";
 fakeImage.addEventListener("load", function() {
   jumboImg.classList.remove("loading");
 });
+
+// Scroll Reveal
+let slideLeft = {
+  distance: "150%",
+  origin: "left",
+  opacity: 0,
+  delay: "500",
+  reset: true,
+};
+let slideRight = {
+  distance: "150%",
+  origin: "right",
+  opacity: 0,
+  delay: "500",
+  reset: true,
+};
+
+ScrollReveal().reveal("#about .container", slideLeft);
+ScrollReveal().reveal("#projects .container", slideRight);
+ScrollReveal().reveal("#experience .container", slideLeft);
+ScrollReveal().reveal("#tech .container", slideRight);
