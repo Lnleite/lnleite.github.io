90bb6d049c3c103f22bdf635b6085c82582921b0
lndubose <lndubose13@gmail.com>
2019-01-23 17:36:00 -0600

    Delete add event listener
    Got rid of the onLoad for the fakeImage because it was throwing an error
    and it was not needed.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
-let jumboImg = document.querySelector(".jumboImg");
+let jumboImg = document.querySelector('.jumboImg');
 
 const fakeImage = new Image();
-fakeImage.onload = function() {
-  jumboImg.styles.backgroundImage = `url(${fakeImage.src})`;
-};
-fakeImage.src = "images/jumboImg.jpg";
+fakeImage.src = 'images/jumboImg.jpg';
 
-fakeImage.addEventListener("load", function() {
-  jumboImg.classList.remove("loading");
+fakeImage.addEventListener('load', function() {
+    jumboImg.classList.remove('loading');
 });
 
 // Scroll Reveal
 let slideLeft = {
-  distance: "150%",
-  origin: "left",
-  opacity: 0,
-  delay: "400",
+    distance: '150%',
+    origin: 'left',
+    opacity: 0,
+    delay: '400',
 };
 let slideRight = {
-  distance: "150%",
-  origin: "right",
-  opacity: 0,
-  delay: "400",
+    distance: '150%',
+    origin: 'right',
+    opacity: 0,
+    delay: '400',
 };
 
-ScrollReveal().reveal("#about .container", slideLeft);
-ScrollReveal().reveal("#projects .container", slideRight);
-ScrollReveal().reveal("#experience .container", slideLeft);
-ScrollReveal().reveal("#tech .container", slideRight);
+ScrollReveal().reveal('#about .container', slideLeft);
+ScrollReveal().reveal('#projects .container', slideRight);
+ScrollReveal().reveal('#experience .container', slideLeft);
+ScrollReveal().reveal('#tech .container', slideRight);
