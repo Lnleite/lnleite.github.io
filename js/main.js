9db7030e3ecbcbdfe170683f923f2141752e894f
Lauren D <lndubose13@gmail.com>
2019-02-23 23:39:59 -0600

    Fish animation, more mobile friendly
    - added the angler fish animation, still need to make it more mobile friendly
    - Only need to work on experience before done with the sections
    - So left todo: finish mobile friendly, fix nav, add fish in background

--- js/main.js
+++ js/main.js
@@ -1,27 +1,45 @@
-let jumboImg = document.querySelector('.jumboImg');
+let jumboImg = document.querySelector(".jumboImg");
 
 const fakeImage = new Image();
-fakeImage.src = 'images/jumboImg.jpg';
+fakeImage.src = "images/jumboImg.jpg";
 
-fakeImage.addEventListener('load', function() {
-    jumboImg.classList.remove('loading');
+fakeImage.addEventListener("load", function() {
+  jumboImg.classList.remove("loading");
 });
 
 // Scroll Reveal
 let slideLeft = {
-    distance: '150%',
-    origin: 'left',
-    opacity: 0,
-    delay: '400',
+  distance: "150%",
+  origin: "left",
+  opacity: 0,
+  delay: "400",
 };
 let slideRight = {
-    distance: '150%',
-    origin: 'right',
-    opacity: 0,
-    delay: '400',
+  distance: "150%",
+  origin: "right",
+  opacity: 0,
+  delay: "400",
 };
 
-ScrollReveal().reveal('#about .container', slideLeft);
-ScrollReveal().reveal('#projects .container', slideRight);
-ScrollReveal().reveal('#experience .container', slideLeft);
-ScrollReveal().reveal('#tech .container', slideRight);
+ScrollReveal().reveal("#about .container", slideLeft);
+ScrollReveal().reveal("#projects .container", slideRight);
+ScrollReveal().reveal("#experience .container", slideLeft);
+ScrollReveal().reveal("#tech .container", slideRight);
+
+// Fish
+let fish = document.querySelector(".fish");
+let eye = document.querySelector(".eye");
+
+function swim() {
+  fish.classList.add("swim");
+}
+
+fish.addEventListener("mouseover", function(event) {
+  eye.classList.add("openeye");
+  TweenLite.to(fish, 20, {
+    onStart: swim,
+    ease: Circ.ease,
+    x: 5000,
+    delay: 1,
+  });
+});
